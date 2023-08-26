5166c8264ff12a42b13aece09b1e9947e6e9d860
abhijit barman <abhijitbarman96@gmail.com>
2023-08-26 23:37:52 +0530

    day3

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# hulking-income-7436
-
-Api_url: https://hulking-income-7436.onrender.com/
--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Dashboard } from "../pages/Dashboard"
+import { Home } from "../pages/Home"
+import { Login } from "../pages/Login"
+import { PageNotFound } from "../pages/PageNotFound"
 
 export const AllRoutes = () => {
+
   return (
-    <div>AllRoutes</div>
+    <div>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </div>
   )
 }
