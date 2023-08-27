b7bd5de978fa7b3bb37014bd1bd7f857e0f3a07e
abhijit barman <abhijitbarman96@gmail.com>
2023-08-27 19:32:40 +0530

    day4

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
-export const AuthContextProvider = () => {
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false)
+  const [name, setName] = useState("user")
+
+  const login = (name) => {
+    // console.log(name);
+    setIsAuth(true)
+    setName(name)
+  }
+
+  const logout = () => {
+    setIsAuth(false)
+  }
+
+  const providerState = {
+    login: login,
+    logout: logout,
+    isAuth: isAuth,
+    name: name,
+    setIsAuth
+  }
+
   return (
-    <div>AuthContextProvider</div>
+    <AuthContext.Provider value={providerState}>
+      {children}
+    </AuthContext.Provider>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
+import {
+  Flex,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { AuthContext } from '../context/AuthContextProvider';
 
 export const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate()
+  const { login, logout, isAuth, setIsAuth } = useContext(AuthContext)
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const logindata = {
+      username: username,
+      password: password
+    };
+
+    axios.get('https://hulking-income-7436.onrender.com/login', logindata)
+      .then(res => {
+        if (res.status) {
+          // console.log("success");
+          if (checkuser(res?.data, logindata)) {
+            login(logindata.username)
+            // console.log(username);
+            // setIsAuth(true)
+            navigate(`/`)
+          }
+          // console.log(res?.data)
+          // return <Navigate to="/" />;
+        }
+      })
+      .catch(error => {
+        console.error('Login failed:', error);
+      });
+  };
+
+
+  function checkuser(user, inputData) {
+    for (let i = 0; i < user.length; i++) {
+      // console.log(user[i]===inputData.username);
+      if (user[i].username === inputData.username && user[i].password === inputData.password) {
+        if (inputData.username === "admin" && inputData.password === "admin") {
+          // console.log("admin");
+          // return <Navigate to="/"/>
+          login("Admin")
+          navigate(`/dashboard`)
+          return
+        }
+        // console.log(true);
+        // navigate(`/`)
+        // login()
+        return true;
+      }
+    }
+    // console.log(false);
+    return false
+  }
+
+
+  // if(isAuth){
+  //   return <Navigate to="/"/>
+  // }
+
+  return (
+    <Flex align="center" justify="center" minH="100vh">
+      <Box bg="gray.100" p={8} rounded="md" boxShadow="md" w={{ base: '90%', sm: '400px' }}>
+        <form onSubmit={handleLogin}>
+          <FormControl>
+            <FormLabel>Username</FormLabel>
+            <Input
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              mb={4}
+            />
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              mb={4}
+            />
+            <Button type="submit" colorScheme="teal" w="100%">
+              Log In
+            </Button>
+          </FormControl>
+        </form>
+      </Box>
+    </Flex>
+  );
+};
